6deec2c789333d3fd7752090b0b58992480b962b
Leo Orellana <leoorellana@lawn-143-215-58-205.lawn.gatech.edu>
2018-03-10 09:10:18 -0500

    final update

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,28 +1,140 @@
-// here we ask user for his/her name
-var userName = prompt("What is your name?");
 
 
-//$(".container").text(RandomNumber);
 
-var crystals = $("#images");
-var Wins = 0;
-var Losses = 0;
-var Final = "";
-var counter = 0;
+// variable declations
+
+var userTotal= 0;
+var wins = 0;
+var losses = 0;
+var final = "";
+
+var valueImg1 = 0;
+var valueImg2 = 0;
+var valueImg3 = 0;
+var valueImg4 = 0;
 
 
 
 
 $(document).ready(function() {
 
-    // Create a string which will hold the lottery number
-    var RandomNumber = Math.floor(Math.random() * 120) + 19;
+    // Create a Random number as target number for users to begin game
+    var randomNumber = Math.floor(Math.random() * 101) + 19;
 
-    console.log(RandomNumber);
+    console.log(randomNumber);
 
     // ... and then dump the random number into our random-number div.
-    $("#numberToGuess").text(RandomNumber);
+    $("#numberToGuess").text(randomNumber);
+
+    // here we create our random numbers for each crystal of 1-12..
+    valueImg1 = Math.floor(Math.random() * 12) + 1;
+    valueImg2 = Math.floor(Math.random() * 12) + 1;
+    valueImg3 = Math.floor(Math.random() * 12) + 1;
+    valueImg4 = Math.floor(Math.random() * 12) + 1;
+
+    console.log(valueImg1);
+
+    //appending losses and wins onto the DOM..
+
+    $("#Wins").append(wins);
+    $("#Losses").append(losses);
+
+    //resetting our game...
+
+ function reset() {
+     randomNumber = Math.floor(Math.random() * 101) + 19;
+     console.log(randomNumber);
+
+     $("#numberToGuess").text(randomNumber);
+
+     valueImg1 = Math.floor(Math.random() * 12) + 1;
+     valueImg2 = Math.floor(Math.random() * 12) + 1;
+     valueImg3 = Math.floor(Math.random() * 12) + 1;
+     valueImg4 = Math.floor(Math.random() * 12) + 1;
+     userTotal = 0;
+
+     // adding our final score to final div...
+     $("#Final-Score").text(final);
+ }
 
 
-});
+    //setting up the on-click command for crystals
+    $('#images1').on ('click', function() {
+        userTotal = userTotal + valueImg1;
+        console.log("Final-Score= " + userTotal);
+        $('#Final-Score').text(userTotal);
+        if(userTotal === randomNumber){
+            wins++;
+            $("#Wins").text(wins);
+            alert("Congrats! Target Score Met");
+            reset();
+        }
+        else if(userTotal > randomNumber){
+            losses++;
+            $("#Losses").text(losses);
+            alert("Try Again");
+            reset();
+        }
+
+    });
+
+    $('#images2').on ('click', function() {
+        userTotal = userTotal + valueImg2;
+        console.log("Final-Score= " + userTotal);
+        $('#Final-Score').text(userTotal);
+        if(userTotal === randomNumber){
+            wins++;
+            $("#Wins").text(wins);
+            alert("Congrats! Target Score Met");
+            reset();
+        }
+        else if(userTotal > randomNumber){
+            losses++;
+            $("#Losses").text(losses);
+            alert("Try Again");
+            reset();
+        }
+
+    });
+
+    $('#images3').on ('click', function() {
+        userTotal = userTotal + valueImg3;
+        console.log("Final-Score= " + userTotal);
+        $('#Final-Score').text(userTotal);
+        if(userTotal === randomNumber){
+            wins++;
+            $("#Wins").text(wins);
+            alert("Congrats! Target Score Met");
+            reset();
+        }
+        else if(userTotal > randomNumber){
+            losses++;
+            $("#Losses").text(losses);
+            alert("Try Again");
+            reset();
+        }
+
+    });
+
+    $('#images4').on ('click', function() {
+        userTotal = userTotal + valueImg4;
+        console.log("Final-Score= " + userTotal);
+        $('#Final-Score').text(userTotal);
+        if(userTotal === randomNumber){
+            wins++;
+            $("#Wins").text(wins);
+            alert("Congrats! Target Score Met");
+            reset();
+        }
+        else if(userTotal > randomNumber){
+            losses++;
+            $("#Losses").text(losses);
+            alert("Try Again");
+            reset();
+        }
+
+    });
+
+
+ });
 
